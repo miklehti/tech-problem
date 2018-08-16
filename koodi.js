f79ba8cedbe821de048d9e31fb222602ba967fb5
Mikko Lehtinen <mikkoleh@office.nbl.fi>
2018-08-16 08:49:25 +0300

    lopullinen versio

--- koodi.js
+++ koodi.js
@@ -1,15 +1,24 @@
-//LinkStations, oletan että on 'pysyvämpi'
+//LinkStations, nyt näin mutta voisi tulla palvelimelta
 const linkStations = [[0, 0, 10],[20, 20, 5], [10, 0, 12]];
+//testejä
+//const linkStations = [[20, 20, 5], [0, 0, 10], [10, 0, 12]];
+//const linkStations = [[10, 0, 12], [20, 20, 5], [0, 0, 10]];
+//const linkStations = [[0, 0, 10],[20, 20, 5], [10, 0, 12], [5, 5, 18]];
+//const linkStations = [[5, 5, 18], [0, 0, 10],[20, 20, 5], [10, 0, 12]];
 
-//nyt syote tälläin, voisi tulla jostain muualta, oletan että tämä vaihtelee useammin
+//nyt syöte tälläin, voisi tulla jostain muualta, oletan että tämä vaihtelee useammin
 const input = [[0, 0], [100, 100], [15,10], [18, 18]];
+//testejä
+//const input = [[5, 5], [100, 100], [15,10], [18, 18]];
+//const input = [[4, 0], [100, 100], [15,10], [18, 18]];
+//const input = [[0, 0], [100, 100], [15,10], [18, 18], [3, 3] ];
 
-//lasketaaEtaisyys
-const calculateDistance = (x1, y1, x2, y2) => {
-  return Math.sqrt( Math.pow( (x2 -x1), 2) + Math.pow( (y2 -y1), 2) );
+//lasketaaEtaisyys kahden koordinaatin välillä
+const calculateDistance = (coordinates) => {
+  return Math.sqrt( Math.pow( (coordinates[2] - coordinates[0]), 2) + Math.pow( ( coordinates[3] - coordinates[1] ), 2) );
 };
 
-//laske power
+//laske power ohjeen mukaan
 const calculatePower = (reach, distance) => {
   if(distance>reach){
     return 0;
@@ -18,35 +27,36 @@ const calculatePower = (reach, distance) => {
   }
 }
 
-//laske power kun ei tiedetä etäisyyttä
-const getPower = (reach, x1, y1, x2, y2) => {
-  return   calculatePower(reach, calculateDistance(x1, y1, x2, y2));
+//laske power kun ei tiedetä etäisyyttä vielä
+const getPower = (reach, ...coordinates) => {
+  return   calculatePower(reach, calculateDistance(coordinates));
 }
 
-//haetaan tulos ja tulostetaan
-const printInput = (x, y) => {
-  const result = linkStations
-    .filter(station => {
-      return   getPower(station[2], x, y, station[0], station[1]) > 0;
-    })
-    .reduce((prev, next) => {
-       if (getPower(prev[2], x, y, prev[0], prev[1]) > getPower(next[2], x, y, next[0], next[1])){
-         return prev;
-       }
-      return next;
-    }, 0);
-
-  console.log(result);
-  if(result!==0) {
-  //  return console.log(`Best link station for point ${x},${y} is ${result[0]},${result[1]} with power ${calculatePower(result[2], calculateDistance(x, y, result[0], result[1]))}`);
-    return console.log(`Best link station for point ${x},${y} is ${result[0]},${result[1]} with power ${getPower(result[2],x, y, result[0], result[1])}`);
-  } else {
-    return console.log(`No link station within reach for point ${x},${y}`);
-  }
-};
+// palauttaa stationin josta eniten poweria
+const getStation = (x, y) => {
+  return linkStations
+      // poistetaan ne stationit jotka ovat kantaman ulkopuolella
+      .filter(station => {
+        return getPower(station[2], x, y, station[0], station[1]) > 0;
+      })
+      // vertaillaan millä stationilla on eniten poweria, en tiedä voisiko tähänkin survoa tuon filterin. Ehkäpä se riskein kohta...
+      .reduce((prev, next) => {
+         if (getPower(prev[2], x, y, prev[0], prev[1]) > getPower(next[2], x, y, next[0], next[1])){
+           return prev;
+         }
+        return next;
+      }, 0);
+}
 
 
-//kutsutaan vuoorotellen jokaisella syötteellä tulostusta
+
+//1. aloitus kutsutaan vuorotellen jokaisella syötteellä ja tulostetaan
 input.map(coordinates => {
-     return printInput(coordinates[0], coordinates[1]);
+  const result = getStation(coordinates[0], coordinates[1]);
+  if(result!==0) {
+    return console.log(`Best link station for point ${coordinates[0]},${coordinates[1]} is ${result[0]},${result[1]} with power ${getPower(result[2],coordinates[0], coordinates[1], result[0], result[1])}`);
+  } else {
+    return console.log(`No link station within reach for point ${coordinates[0]},${coordinates[1]}`);
+  }
+
 });
--- playground.js
+++ playground.js
@@ -12,3 +12,35 @@ console.log(laskeEtaisyys(0, 0, 0, 10));
 toimii kait
 
 */
+/*
+ges6 = years.map((el, index) => {
+    const now = new Date().getFullYear();
+    const age = now - el;
+    return `Age element ${index + 1}: ${age}.`
+});
+
+laskenta järjestys:
+1. laskeEtaisyys
+2. laske power
+3.
+*/
+
+
+/* ehkä
+
+/*
+.filter(station => {
+return   calculatePower(station[2], calculateDistance(x, y, station[0], station[1])) > 0;
+})
+*/
+/* toimii varmasti
+//laske power kun ei tiedetä etäisyyttä vielä
+const getPower = (reach, x1, y1, x2, y2) => {
+  return   calculatePower(reach, calculateDistance(x1, y1, x2, y2));
+}
+
+//lasketaaEtaisyys kahden koordinaatin välillä
+const calculateDistance = (x1, y1, x2, y2) => {
+  return Math.sqrt( Math.pow( (x2 -x1), 2) + Math.pow( (y2 -y1), 2) );
+};
+*/
