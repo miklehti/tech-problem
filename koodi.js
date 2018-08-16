85f30e0dcb9eb48d6f4df0c4e8fd60d545e65819
Mikko Lehtinen <mikkoleh@office.nbl.fi>
2018-08-16 09:15:57 +0300

    comments in english

--- koodi.js
+++ koodi.js
@@ -1,4 +1,4 @@
-//LinkStations, nyt näin mutta voisi tulla palvelimelta
+//LinkStations, from instructions, could be also from a server...
 const linkStations = [[0, 0, 10],[20, 20, 5], [10, 0, 12]];
 //testejä
 //const linkStations = [[20, 20, 5], [0, 0, 10], [10, 0, 12]];
@@ -6,19 +6,19 @@ const linkStations = [[0, 0, 10],[20, 20, 5], [10, 0, 12]];
 //const linkStations = [[0, 0, 10],[20, 20, 5], [10, 0, 12], [5, 5, 18]];
 //const linkStations = [[5, 5, 18], [0, 0, 10],[20, 20, 5], [10, 0, 12]];
 
-//nyt syöte tälläin, voisi tulla jostain muualta, oletan että tämä vaihtelee useammin
+//input from instructions,  could be also from a server...
 const input = [[0, 0], [100, 100], [15,10], [18, 18]];
 //testejä
 //const input = [[5, 5], [100, 100], [15,10], [18, 18]];
 //const input = [[4, 0], [100, 100], [15,10], [18, 18]];
 //const input = [[0, 0], [100, 100], [15,10], [18, 18], [3, 3] ];
 
-//lasketaaEtaisyys kahden koordinaatin välillä
+//distance betveen two points
 const calculateDistance = (coordinates) => {
   return Math.sqrt( Math.pow( (coordinates[2] - coordinates[0]), 2) + Math.pow( ( coordinates[3] - coordinates[1] ), 2) );
 };
 
-//laske power ohjeen mukaan
+//calculate power according to instructions
 const calculatePower = (reach, distance) => {
   if(distance>reach){
     return 0;
@@ -27,19 +27,19 @@ const calculatePower = (reach, distance) => {
   }
 }
 
-//laske power kun ei tiedetä etäisyyttä vielä
+//calculate power when distance not known
 const getPower = (reach, ...coordinates) => {
   return   calculatePower(reach, calculateDistance(coordinates));
 }
 
-// palauttaa stationin josta eniten poweria
+// return the station that has the most power
 const getStation = (x, y) => {
   return linkStations
-      // poistetaan ne stationit jotka ovat kantaman ulkopuolella
+      // remove stations that are out of reach.
       .filter(station => {
         return getPower(station[2], x, y, station[0], station[1]) > 0;
       })
-      // vertaillaan millä stationilla on eniten poweria, en tiedä voisiko tähänkin survoa tuon filterin. Ehkäpä se riskein kohta...
+      // compare which of the remaining stations has the most power. Probably the most risky part, hope it works like I planned it.
       .reduce((prev, next) => {
          if (getPower(prev[2], x, y, prev[0], prev[1]) > getPower(next[2], x, y, next[0], next[1])){
            return prev;
@@ -48,9 +48,7 @@ const getStation = (x, y) => {
       }, 0);
 }
 
-
-
-//1. aloitus kutsutaan vuorotellen jokaisella syötteellä ja tulostetaan
+//1. starting point, for each cordinate get the station and print output.
 input.map(coordinates => {
   const result = getStation(coordinates[0], coordinates[1]);
   if(result!==0) {
